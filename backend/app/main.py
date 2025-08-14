a1493628f15de2fccff8ddc85da625f89087e18a
Rajotiana <rajotiananyandryrakotondrazafy@gmail.com>
2025-08-14 19:20:28 +0300

    .gitignore

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,7 @@
 from fastapi import FastAPI
 from .api import routers as api_routers
-from .db.base import Base
-from .db.session import engine
 app = FastAPI(title="Gestion AO")
 
-# Création automatique des tables selon les modèles
-Base.metadata.create_all(bind=engine)
 for r in api_routers:
     app.include_router(r)
 @app.get("/")
